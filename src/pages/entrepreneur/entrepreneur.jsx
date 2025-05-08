734a40669874e6fe2de8dadcb20dde3fa6ffbf08
praseen17 <178655392+praseen17@users.noreply.github.com>
2025-05-08 18:46:43 +0530

    Update entrepreneur.jsx

--- src/pages/entrepreneur/entrepreneur.jsx
+++ src/pages/entrepreneur/entrepreneur.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./entrepreneur.css";
 import { FaLightbulb, FaChartLine, FaNetworkWired, FaHandshake } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Entrepreneur = () => {
   return (
