1cbf9f73fc21b297d1cc3007f2d9848d43eba2bb
praseen17 <178655392+praseen17@users.noreply.github.com>
2025-05-08 18:36:07 +0530

    Update CourseDescription.jsx

--- src/pages/coursedescription/CourseDescription.jsx
+++ src/pages/coursedescription/CourseDescription.jsx
@@ -8,14 +8,13 @@ import toast from "react-hot-toast";
 import { UserData } from "../../context/UserContext";
 import Loading from "../../components/loading/Loading";
 
-const CourseDescription = ({ user }) => {
+const CourseDescription = () => {
   const params = useParams();
   const navigate = useNavigate();
-
+  const { user, isAuth } = UserData();
   const [loading, setLoading] = useState(false);
 
   const { fetchUser } = UserData();
-
   const { fetchCourse, course, fetchCourses, fetchMyCourse } = CourseData();
 
   useEffect(() => {
@@ -23,66 +22,76 @@ const CourseDescription = ({ user }) => {
   }, []);
 
   const checkoutHandler = async () => {
+    if (!isAuth) {
+      toast.error("Please login to purchase the course");
+      navigate("/login");
+      return;
+    }
+
     const token = localStorage.getItem("token");
     setLoading(true);
 
-    const {
-      data: { order },
-    } = await axios.post(
-      `${server}/api/course/checkout/${params.id}`,
-      {},
-      {
-        headers: {
-          token,
-        },
-      }
-    );
-
-    const options = {
-      key: "rzp_test_yOMeMyaj2wlvTt", // Enter the Key ID generated from the Dashboard
-      amount: order.id, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-      currency: "INR",
-      name: "Vhass", //your business name
-      description: "Learn with us",
-      order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-
-      handler: async function (response) {
-        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-          response;
-
-        try {
-          const { data } = await axios.post(
-            `${server}/api/verification/${params.id}`,
-            {
-              razorpay_order_id,
-              razorpay_payment_id,
-              razorpay_signature,
-            },
-            {
-              headers: {
-                token,
-              },
-            }
-          );
-
-          await fetchUser();
-          await fetchCourses();
-          await fetchMyCourse();
-          toast.success(data.message);
-          setLoading(false);
-          navigate(`/payment-success/${razorpay_payment_id}`);
-        } catch (error) {
-          toast.error(error.response.data.message);
-          setLoading(false);
+    try {
+      const {
+        data: { order },
+      } = await axios.post(
+        `${server}/api/course/checkout/${params.id}`,
+        {},
+        {
+          headers: {
+            token,
+          },
         }
-      },
-      theme: {
-        color: "#8a4baf",
-      },
-    };
-    const razorpay = new window.Razorpay(options);
+      );
 
-    razorpay.open();
+      const options = {
+        key: "rzp_test_yOMeMyaj2wlvTt",
+        amount: order.id,
+        currency: "INR",
+        name: "Vhass",
+        description: "Learn with us",
+        order_id: order.id,
+
+        handler: async function (response) {
+          const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+            response;
+
+          try {
+            const { data } = await axios.post(
+              `${server}/api/verification/${params.id}`,
+              {
+                razorpay_order_id,
+                razorpay_payment_id,
+                razorpay_signature,
+              },
+              {
+                headers: {
+                  token,
+                },
+              }
+            );
+
+            await fetchUser();
+            await fetchCourses();
+            await fetchMyCourse();
+            toast.success(data.message);
+            setLoading(false);
+            navigate(`/payment-success/${razorpay_payment_id}`);
+          } catch (error) {
+            toast.error(error.response.data.message);
+            setLoading(false);
+          }
+        },
+        theme: {
+          color: "#8a4baf",
+        },
+      };
+      const razorpay = new window.Razorpay(options);
+      razorpay.open();
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -110,7 +119,7 @@ const CourseDescription = ({ user }) => {
 
               <p>Let's get started with course At ₹{course.price}</p>
 
-              {user && user.subscription.includes(course._id) ? (
+              {isAuth && user && user.subscription.includes(course._id) ? (
                 <button
                   onClick={() => navigate(`/course/study/${course._id}`)}
                   className="common-btn"
@@ -119,7 +128,7 @@ const CourseDescription = ({ user }) => {
                 </button>
               ) : (
                 <button onClick={checkoutHandler} className="common-btn">
-                  Buy Now
+                  {isAuth ? "Buy Now" : "Login to Purchase"}
                 </button>
               )}
             </div>
