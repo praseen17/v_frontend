efddb1583226cc387cd052fbffcb6bb0047b3de7
praseen17 <178655392+praseen17@users.noreply.github.com>
2025-05-08 18:46:02 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
